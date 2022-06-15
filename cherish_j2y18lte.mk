b22d06dbf19daee9d4f8a77c9976cb5498c905bd
nnippon <adameyyad999@gmail.com>
2022-06-16 00:53:28 +0900

    Build With FOD Animations

--- cherish_j2y18lte.mk
+++ cherish_j2y18lte.mk
@@ -18,6 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+TARGET_WANTS_FOD_ANIMATIONS := true
 
 # Inherit from j2y18lte device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
